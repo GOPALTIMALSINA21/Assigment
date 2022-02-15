d242fd5230009e0b6857e24f32f4250a99565cb8
GOPALTIMALSINA21 <99704543+GOPALTIMALSINA21@users.noreply.github.com>
2022-02-15 14:29:34 +0545

    Add files via upload

---
+++ Rectangular.java
@@ -0,0 +1,15 @@
+public class Rectangular {
+	public static void main(String[] args) {
+		// Area of rectangular
+		//Formula=l*b
+		float length = 4;
+		float breath = 5;
+		float area;
+		area = length * breath;
+		System.out.println("area of rectangular");
+		System.out.println(area);
+		
+				
+		
+	}
+}
---
+++ RectangularPattern.java
@@ -0,0 +1,14 @@
+
+public class RectangularPattern {
+	
+	public static void main(String[] args) {
+		System.out.println("*****");
+		System.out.println("****");
+		System.out.println("***");
+		System.out.println("**");
+		System.out.println("*");
+		
+	}
+	
+
+}
